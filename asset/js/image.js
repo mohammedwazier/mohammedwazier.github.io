46358a2fb12ee6ce65c49ee838ddf741a8e2b518
Masihkasar <m.waziruddin@gmail.com>
2021-02-01 00:53:42 +0700

    Deelting Project

--- asset/js/image.js
+++
@@ -1,7 +0,0 @@
-$('.popupVal').click(function(){
-$('body').css("overflow","hidden");
-var valueImage=$(this).find("img").attr('src');
-console.log($(this).find("img").attr('src'));
-$("#blackPage").find("img").attr("src",valueImage);
-$("#blackPage").fadeIn(100);
-});
